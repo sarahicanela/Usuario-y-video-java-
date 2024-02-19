8e4e5ce7574035ef6d973d56c98448f072e8ec20
Sarahicanela669@gmail.com <raymu@192.168.0.77>
2024-02-18 22:16:29 -0600

    USUARIO Y VIDEO JAVA

--- src/main/java/com/mycompany/youtube/Youtube.java
+++ src/main/java/com/mycompany/youtube/Youtube.java
@@ -15,6 +15,10 @@ public class Youtube {
         Video video = new Video ("aprendiendo java", 2400,"http;//sarahi.co" );
         usuario.subirVideo(video);
         usuario.mostrarInfoUsuario();
+        
+         Usuario usuario2=new Usuario("Amiguito");
+        Comentario comment1=new Comentario(":quiero un arroz con leche ",usuario2);
+        System.out.println("El usuario "+ usuario2.getNombre()+ "  publicó el comentario "+ comment1.getContenido() );
     }
 }
 
